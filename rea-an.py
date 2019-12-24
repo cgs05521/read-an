2224161fb65935d325b665a7005af885394ad3cd
bo cheng <cgs05521@gmail.com>
2019-12-24 14:25:56 +0800

    calc reviews

--- rea-an.py
+++ rea-an.py
@@ -1,11 +1,14 @@
+
+sum_lan = 0
 data = []
 count = 0
 with open('reviews.txt','r') as f:
 	for line in f :
 		data.append(line)
-		count += 1
-		if count % 1000 ==0:
-			print(len(data))
 
-print(data)
+for d in data:
+	sum_lan += len(d)
+
+print('留言平均長度為：',sum_lan/len(data),'筆資料')
+
 
